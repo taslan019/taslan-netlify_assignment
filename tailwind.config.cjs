e248bd68139340796ef83fe72d2bb64b8592e843
taslan019 <aslamdraker@gmail.com>
2022-09-29 10:51:56 +0700

    complated all assigment

---
+++ tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+    theme: {
+      screens: {
+        'md': '320x',
+        // => @media (min-width: 640px) { ... }
+  
+        'lg': '1080px',
+        // => @media (min-width: 1024px) { ... }
+      },
+    }
+  }
